23b099c9822e53a8a4abad6928236ad6a2db27bd
Pon4ik123 <taranenkozahar@gmail.com>
2024-06-08 18:43:33 +0200

    lab6

--- src/Lab6/Main.java
+++ src/Lab6/Main.java
@@ -1,5 +1,6 @@
 package Lab6;
 
+import java.sql.PreparedStatement;
 import java.util.*;
 
 public class Main {
@@ -18,15 +19,17 @@ public class Main {
         return bin;
     }
 
-    public static void haming7_4(int n, int k, boolean triger) {
-        List<Integer> originalBites = stringToBinary("zut");
+    public static void haming7_4(int n, int k, String word, int triger) {
+        List<Integer> originalBites = stringToBinary(word);
         List<Integer> tempBits = new ArrayList<>(originalBites);
 
+        System.out.println();
         System.out.print("Entered bites: " + tempBits + "\n");
 
         int x1 = tempBits.get(0) ^ tempBits.get(1) ^ tempBits.get(3);
         int x2 = tempBits.get(0) ^ tempBits.get(2) ^ tempBits.get(3);
         int x4 = tempBits.get(1) ^ tempBits.get(2) ^ tempBits.get(3);
+        System.out.println();
         System.out.println("Even bites: " + x1 + x2 + x4);
 
         List<Integer> result = new ArrayList<>(Collections.nCopies(n, 0));
@@ -37,9 +40,10 @@ public class Main {
         result.set(4, tempBits.get(1));
         result.set(5, tempBits.get(2));
         result.set(6, tempBits.get(3));
+        System.out.println();
         System.out.println("Chosen word: " + result);
 
-        if (triger) {
+        if (triger == 1) {
             int randIndex = new Random().nextInt(result.size() - 1);
             while (randIndex == 0 || randIndex == 1 || randIndex == 3) {
                 randIndex = new Random().nextInt(result.size() - 1);
@@ -67,14 +71,16 @@ public class Main {
                 result.set(s - 1, 0);
             } else result.set(s - 1, 1);
             System.out.println("Chosen word: " + result);
-            System.out.println("Done: " + result.get(2) + result.get(4) + result.get(5) + result.get(6));
         }
+        System.out.println();
+        System.out.println("Result: " + result.get(2) + result.get(4) + result.get(5) + result.get(6));
     }
 
-    public static void haming15_11(int n, int k, boolean triger) {
-        List<Integer> originalBites = stringToBinary("hello");
+    public static void haming15_11(int n, int k, String string, int triger) {
+        List<Integer> originalBites = stringToBinary(string);
         List<Integer> tempBits = new ArrayList<>(originalBites);
 
+        System.out.println();
         System.out.print("Entered bites: " + tempBits + "\n");
 
         List<Integer> M = new ArrayList<>();
@@ -87,110 +93,140 @@ public class Main {
                 K.add(tempBits.get(i));
             }
         }
-        System.out.println("M: " + M);
-        System.out.println("K: " + K);
 
         List<Integer> word = new ArrayList<>();
         word.addAll(M);
         word.addAll(K);
+        System.out.println();
         System.out.println("Coded word: " + word);
 
-        int[][] I = new int[15][15];
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                if (i == j) {
-                    I[i][j] = 1;
-                }
-            }
+        int[][] I = new int[k][k];
+        for (int i = 0; i < I.length; i++) {
+            I[i][i] = 1;
         }
-//        for (int i = 0; i < 15; i++) {
-//            for (int j = 0; j < 15; j++) {
-//                System.out.print(I[i][j]);
-//            }
-//            System.out.println();
-//        }
 
         int [][] P = {{1, 1, 0, 0},
-                      {1, 0, 1, 0},
-                      {0, 1, 1, 0},
-                      {1, 1, 1, 0},
-                      {1, 0, 0, 1},
-                      {0, 1, 0, 1},
-                      {1, 1, 0, 1},
-                      {0, 0, 1, 1},
-                      {1, 0, 1, 1},
-                      {0, 1, 1, 1},
-                      {1, 1, 1, 1}};
+                {1, 0, 1, 0},
+                {0, 1, 1, 0},
+                {1, 1, 1, 0},
+                {1, 0, 0, 1},
+                {0, 1, 0, 1},
+                {1, 1, 0, 1},
+                {0, 0, 1, 1},
+                {1, 0, 1, 1},
+                {0, 1, 1, 1},
+                {1, 1, 1, 1}};
 
-        int[][] G = new int[P.length + I.length][];
-        for (int i = 0; i < G.length; i++) {
-            G[i] = new int[P[0].length + I[0].length];
-        }
-
-        for (int i = 0; i < P.length; i++) {
-            for (int j = 0; j < P[i].length; j++) {
+        int[][] G = new int[k][k + P[0].length];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < P[0].length; j++) {
                 G[i][j] = P[i][j];
             }
-        }
-
-        for (int i = 0; i < I.length; i++) {
-            for (int j = 0; j < I[i].length; j++) {
+            for (int j = 0; j < k; j++) {
                 G[i][j + P[0].length] = I[i][j];
             }
         }
 
-        int[][] PT = new int[P[0].length][P.length];
+        int[][] PTranspose = new int[P[0].length][P.length];
         for (int i = 0; i < P.length; i++) {
             for (int j = 0; j < P[0].length; j++) {
-                PT[j][i] = P[i][j];
+                PTranspose[j][i] = P[i][j];
             }
         }
 
         int[][] IN_K = new int[n-k][n-k];
         for (int i = 0; i < IN_K.length; i++) {
-            for (int j = 0; j <IN_K.length; j++) {
+            for (int j = 0; j <IN_K[0].length; j++) {
                 if (i == j) {
                     IN_K[i][j] = 1;
                 }
             }
         }
 
-        int[][] H = new int[IN_K.length + PT.length][IN_K[0].length];
+        int[][] H = new int[IN_K.length + PTranspose.length][];
+        for (int i = 0; i < H.length; i++) {
+            H[i] = new int[IN_K[0].length + PTranspose[0].length];
+        }
+
+        for (int i = 0; i < IN_K.length; i++) {
+            for (int j = 0; j < IN_K[i].length; j++) {
+                H[i][j] = IN_K[i][j];
+            }
+        }
+
+        for (int i = 0; i < PTranspose.length; i++) {
+            for (int j = 0; j < PTranspose[i].length; j++) {
+                H[i][j + IN_K[0].length] = PTranspose[i][j];
+            }
+        }
+
+        int[][] HTranspose = new int[H[0].length][H.length];
         for (int i = 0; i < H.length; i++) {
             for (int j = 0; j < H[0].length; j++) {
-                if (i < IN_K.length) {
-                    H[i][j] = IN_K[i][j];
-                } else {
-                    H[i][j] = PT[i - IN_K.length][j];
-                }
+                HTranspose[j][i] = H[i][j];
             }
         }
 
-        int[][] HT = new int[H[0].length][H.length];
-        for (int i = 0; i < H.length; i++) {
-            for (int j = 0; j < H[0].length; j++) {
-                HT[j][i] = H[i][j];
+        int[] c = new int[G[0].length];
+        for (int i = 0; i < 15; i++) {
+            int sum = 0;
+            for (int j = 0; j < 11; j++) {
+                sum += G[j][i] * K.get(j);
             }
+            c[i] = sum % 2;
         }
-
-        System.out.println(K.size());
-
-        int[] c = new int[G.length];
-        for (int i = 0; i < G.length; i++) {
-            for (int j = 0; j < G[0].length; j++) {
-                c[i] = G[i][j] * K.get(i); // Accumulate the result of element-wise multiplication
-            }
-        }
+        System.out.println();
+        System.out.println("Array c: ");
         System.out.println(Arrays.toString(c));
 
-        int[] s = new int[4];
+        if(triger == 1){
+            int randIndex = new Random().nextInt(c.length - 1) + 4;
+            if (c[randIndex] == 1) {
+                c[randIndex] =  0;
+            } else c[randIndex] = 1;
+            System.out.println();
+            System.out.println("Chosen word after error: " + Arrays.toString(c));
+        }
 
+        int[] s = new int[4];
+        for (int i = 0; i < 4; i++) {
+            int sum = 0;
+            for (int j = 0; j < HTranspose.length; j++) {
+                sum += c[j] * HTranspose[j][i];
+            }
+            s[i] = sum % 2;
+        }
+
+        int syndrom = (int) (s[0] * Math.pow(2, 0) + s[1] * Math.pow(2, 1) + s[2] * Math.pow(2, 2) + s[3] * Math.pow(2, 3));
+        if (syndrom == 0) System.out.println("There is no mistake");
+        else {
+            System.out.println("The mistake is in: " + syndrom + " bit");
+            System.out.println("Making changes...");
+            if (c[syndrom - 1] == 1) {
+                c[syndrom - 1] = 0;
+            } else c[syndrom - 1] = 1;
+            System.out.println("Chosen word after change: " + Arrays.toString(c));
+        }
+        System.out.println();
+        System.out.print("Result: " );
+        for (int i = 4; i < c.length; i++) {
+            System.out.print(c[i] + " ");
+        }
     }
 
 
     public static void main(String[] args) {
-//        haming7_4(7, 0, true);
+        System.out.print("Enter the word for Haming(7, 4): ");
+        String string = new Scanner(System.in).nextLine();
+        System.out.print("Enter [1] if you want to make change one bit, [0] - don't want to: ");
+        int choice = new Scanner(System.in).nextInt();
+        haming7_4(7, 0, string, choice);
 
-        haming15_11(15, 11, false);
+//        System.out.print("Enter the word for Haming(15, 11): ");
+//        String string = new Scanner(System.in).nextLine();
+//        System.out.print("Enter [1] if you want to make change one bit, [0] - don't want to: ");
+//        int choice = new Scanner(System.in).nextInt();
+//        haming15_11(15, 11, string, choice);
+
     }
 }
